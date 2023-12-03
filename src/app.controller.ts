13fdd1f5892f6ee1a9545b83c06c6e6bfa0aea52
tiagojosedasilva <tiago.estudo.desenvolvimento@gmail.com>
2023-12-03 00:23:33 -0300

    criando User com ID gerado automaticamente

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './database/prisma.service';
+import { stringify } from 'querystring';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService
+    ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    const user = await this.prisma.user.create({
+      data: {
+        name: "Tiago José da Silva",
+        email: "tiago@jose",
+        phone: "2222-2222"
+      }
+    });
+    return stringify(user)
   }
 }
